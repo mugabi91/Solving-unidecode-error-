48c0abc58c46b30262d4f99e7782a8809831714d
mugabi91 <146214824+mugabi91@users.noreply.github.com>
2024-02-06 19:37:19 +0300

    Add files via upload

---
+++ unidecodeerror_solver.py
@@ -0,0 +1,13 @@
+import charset_normalizer
+    
+def encoding(path_to_file):
+    
+    """Takes df and returns dict with language and encoding used  """
+    with open(path_to_file,'rb')as raw_data:
+    
+        output_dict = charset_normalizer.detect(raw_data.read())
+        return output_dict['encoding']
+
+
+if __name__ == "__main__":
+    print("This will only run when the script is executed directly.")
